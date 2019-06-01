c3c16dbe36297d381cd683903d3fe630aee6a467
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-06-01 06:16:01 -0300

    Adding logout button to Header
    This file was missing from last commit.
    - Added new logout button, which has an alert forcing the user to confirm the action
    - Fixed the alignment of the central text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,45 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Icon from "react-native-vector-icons/FontAwesome"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import IconFont from "react-native-vector-icons/FontAwesome"
 import AssyncStorage from "@react-native-community/async-storage"
+import { Alert } from "react-native"
 
 import {
-  StatusBar, StatusBarText, DrawerButton, DrawerButtonText
+  StatusBar, StatusBarText, DrawerButton, ExtraButton, TitleContainer
 } from "./styles"
 
 const Header = ({ navigation, hideFilter, title }) => (
   <StatusBar>
     <DrawerButton onPress={() => navigation.openDrawer()}>
-      <Icon name="bars" size={20} />
+      <IconFont name="bars" size={26} />
     </DrawerButton>
 
-    <StatusBarText>{title}</StatusBarText>
+    <TitleContainer>
+      <StatusBarText>{title}</StatusBarText>
+    </TitleContainer>
 
-    <DrawerButton
-      onPress={
-        !hideFilter
-          ? () => {
-            AssyncStorage.removeItem("@token")
-            navigation.navigate("WelcomePage")
+    <ExtraButton
+      onPress={() => {
+        Alert.alert("Confirmation", "Are you sure you want to logout?", [
+          {
+            text: "Cancel",
+            style: "cancel"
+          },
+          {
+            text: "Logout",
+            style: "destructive",
+            onPress: () => {
+              AssyncStorage.clear()
+
+              navigation.navigate("WelcomePage")
+            }
           }
-          : () => {}
-      }
+        ])
+      }}
     >
-      <DrawerButtonText hideFilter={hideFilter}>Logout</DrawerButtonText>
-    </DrawerButton>
+      <Icon name="logout" size={26} />
+    </ExtraButton>
   </StatusBar>
 )
 
